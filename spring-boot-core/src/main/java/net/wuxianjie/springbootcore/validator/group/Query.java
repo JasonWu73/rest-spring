82e3a58c1d2441082d9ce442668ad4e2b1e88e23
吴仙杰 <wuxianjiezh@gmail.com>
2022-03-30 16:09:55 +0800

    增加查询校验分组

---
+++ spring-boot-core/src/main/java/net/wuxianjie/springbootcore/validator/group/Query.java
@@ -0,0 +1,11 @@
+package net.wuxianjie.springbootcore.validator.group;
+
+import javax.validation.groups.Default;
+
+/**
+ * 校验分组：查询。
+ *
+ * @author 吴仙杰
+ */
+public interface Query extends Default {
+}
